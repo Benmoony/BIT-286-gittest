5d1b9ed5dc8f635b956a83df94349d169c604852
DESKTOP-UCO1OM3\Haytham <haythamlion@outlook.com>
2017-04-03 11:44:46 -0700

    readded function

--- Coffee3.0/Coffee3.0/Program.cs
+++ Coffee3.0/Coffee3.0/Program.cs
@@ -10,7 +10,10 @@ namespace Coffee3._0
     {
         static void Main(string[] args)
         {
-            
+            for (int i = 1; i <= 100; i++)
+            {
+                Console.WriteLine("i: {0}", i);
+            }
         }
     }
 }
